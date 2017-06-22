6f153adc06906b9e9c25800031fa29185cfbe73f
Alcwyn <alcwynparker@hotmail.co.uk>
2017-06-22 18:09:27 +0100

    added the IIFE

--- 02-functions/build/js/index.js
+++ 02-functions/build/js/index.js
@@ -7,12 +7,19 @@
 * @Project: Unlocking Potential
 * @Filename: index.js
 * @Last modified by:   alcwynparker
-* @Last modified time: 2017-06-22T17:42:58+01:00
+* @Last modified time: 2017-06-22T18:09:12+01:00
 *
+* in this file we will look at functions and the various new features and techniques
+* that have arrived with es6
 *
 */
 
-// Out with the old, in with the new! (kind of)
+// Arrow Functions
+// ----------------------------------------------------------------
+// Action : Discussion - look at the differences between functions and arrow functions,
+// what are the pros and cons of each?
+
+// Example 1: Standard Function
 var oldLangName = 'ECMAScript (ES5)'; // var is function scope (){} or global
 var oldLangRelDate = 2009;
 
@@ -32,13 +39,10 @@ function outputOldLang(string1, string2) {
 // call function
 console.log(outputOldLang(oldLangName, oldLangRelDate));
 
-// ----------------------------------------------------------------
-// After es6
+// Example 2: Arrow Function
 var newLangName = 'ECMAScript 2015 (ES6)'; // const is block scoped and is read-only
 var newLangRelDate = 2015; // let is block scope {}
 
-// Action : Discussion - look at the differences between functions and arrow functions,
-// what are the pros and cons of each?
 
 // Arrow function
 var outputNewLang = function outputNewLang(string1, string2) {
@@ -47,3 +51,20 @@ var outputNewLang = function outputNewLang(string1, string2) {
 };
 
 console.log(outputNewLang(newLangName, newLangRelDate));
+
+// Imediately-Invoked function Expression (IIFE)
+// ----------------------------------------------------------------
+// Action : Using what you have learnt from the previous activity
+// convert a IIFE into an arrow function (you can use google to help find
+// the answer)
+
+// An IIFE is used to isoloate your script from the rest of the code base.
+// this is especially useful in modules. We will dicuss this in great detail
+// later on. for now it is just best practice to wrap your code in an IIFE to
+// safe guard the code base.
+
+(function () {
+  console.log('I run as soon as the IIFE has been parsed and created.');
+})();
+
+// your turn....
--- 02-functions/src/index.js
+++ 02-functions/src/index.js
@@ -5,15 +5,22 @@
 * @Project: Unlocking Potential
 * @Filename: index.js
 * @Last modified by:   alcwynparker
-* @Last modified time: 2017-06-22T17:42:58+01:00
+* @Last modified time: 2017-06-22T18:09:12+01:00
 *
+* in this file we will look at functions and the various new features and techniques
+* that have arrived with es6
 *
 */
 
-// Out with the old, in with the new! (kind of)
+
+// Arrow Functions
+// ----------------------------------------------------------------
+// Action : Discussion - look at the differences between functions and arrow functions,
+// what are the pros and cons of each?
+
+// Example 1: Standard Function
 var oldLangName = 'ECMAScript (ES5)';    // var is function scope (){} or global
 var oldLangRelDate = 2009
-
 
 /**
  * outputOldLang - function to output display text for language and released
@@ -32,18 +39,34 @@ function outputOldLang(string1, string2){
 console.log(outputOldLang( oldLangName, oldLangRelDate ));
 
 
-// ----------------------------------------------------------------
-// After es6
+// Example 2: Arrow Function
 const newLangName = 'ECMAScript 2015 (ES6)';   // const is block scoped and is read-only
 let newLangRelDate = 2015;           // let is block scope {}
 
-// Action : Discussion - look at the differences between functions and arrow functions,
-// what are the pros and cons of each?
 
 // Arrow function
-let outputNewLang = (string1, string2) => {
+const outputNewLang = (string1, string2) => {
   let output = string1 + ' was released in ' + string2;
   return output;
 };
 
 console.log(outputNewLang(newLangName, newLangRelDate));
+
+
+
+// Imediately-Invoked function Expression (IIFE)
+// ----------------------------------------------------------------
+// Action : Using what you have learnt from the previous activity
+// convert a IIFE into an arrow function (you can use google to help find
+// the answer)
+
+// An IIFE is used to isoloate your script from the rest of the code base.
+// this is especially useful in modules. We will dicuss this in great detail
+// later on. for now it is just best practice to wrap your code in an IIFE to
+// safe guard the code base.
+
+(function() {
+    console.log('I run as soon as the IIFE has been parsed and created.');
+})();
+
+// your turn....
